2ba270c65d0c15a4931a4f008b8300702b7f5b9f
Zoe Edgington <edgingza@rose-hulman.edu>
2024-12-05 09:54:02 -0500

    Added launch for no gui

--- src/bringup/launch/rocko_gui.launch.py
+++ src/bringup/launch/rocko_gui.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "gui",
-            default_value="false",
+            default_value="true",
             description="Start RViz2 automatically with this launch file.",
         )
     )
