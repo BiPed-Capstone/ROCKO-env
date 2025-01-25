431d9afd9226e9214227134480e6d6251fca493c
Zoe Edgington <edgingza@rose-hulman.edu>
2025-01-25 17:54:54 -0500

    changed encoder object

--- rocko_env/rocko_env/hardware/sensors/pid4991/PID4991.py
+++ rocko_env/rocko_env/hardware/sensors/pid4991/PID4991.py
@@ -13,15 +13,15 @@ class PID4991(Node):
     def __init__(self, service_name, addr:int):
         # Handle any hardware initialization here
         i2c = board.I2C()  # uses board.SCL and board.SDA
-        s = seesaw.Seesaw(i2c, addr)
+        self.s = seesaw.Seesaw(i2c, addr)
 
         seesaw_product = (s.get_version() >> 16) & 0xFFFF
         print("Found product {}".format(seesaw_product))
         if seesaw_product != 4991:
             print("Wrong firmware loaded?  Expected 4991")
 
-        self.encoder = rotaryio.IncrementalEncoder(s)
-        self.last_position = 0
+        # self.encoder = rotaryio.IncrementalEncoder(s)
+        # self.last_position = 0
         self.meters_conversion = 53 / (0.144 * np.pi) # 144 mm wheel diameter, 145.1 PPR encoder resolution at gearbox output shaft
 
         # Create a new service to send data to ros2_control
@@ -31,12 +31,17 @@ class PID4991(Node):
 
     def callback(self, request, response):
         # Interact with hardware here based on info in request (if there is any)
-        position = self.encoder.position / self.meters_conversion
+        position = self.s.encoder_position() / self.meters_conversion
+        velocity = self.s.encoder_delta() / self.meters_conversion / 0.01
         response.position = position
-        response.velocity = (position - self.last_position) / 0.01
-        self.last_position = position
+        response.velocity = velocity
+        
+        # position = self.encoder.position / self.meters_conversion
+        # response.position = position
+        # response.velocity = (position - self.last_position) / 0.01
+        # self.last_position = position
 
-        # self.get_logger().info("raw pos: " + str(self.encoder.position))
+        self.get_logger().info("raw pos: " + str(self.encoder.position))
 
         return response
 
