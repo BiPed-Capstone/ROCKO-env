cdfa03a01d09f088fa925f01650595e4583ceb28
elizabeth <elizabeth@ziemer.net>
2025-02-03 09:38:19 -0500

    small bugfix

--- rocko_env/rocko_env/hardware/sensors/icm20948/calibration/run_calibration.py
+++ rocko_env/rocko_env/hardware/sensors/icm20948/calibration/run_calibration.py
@@ -1,6 +1,6 @@
 from calibrate_accelerometer import AccCalibrator
 
 input('Press any key to begin calibration.')
-calibrator = AccCalibrator
+calibrator = AccCalibrator()
 calibrator.capture_dataset()
 calibrator.calibrate_dataset()
